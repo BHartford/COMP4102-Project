d3569d8779e79b61d3bf56295d4aa2e9db98e0b3
BHartford <hartbrand99@gmail.com>
2020-02-20 15:21:12 -0500

    blur stuff

--- Source.cpp
+++ Source.cpp
@@ -4,9 +4,28 @@ using namespace std;
 using namespace cv;
 int main()
 {
-	Mat img = imread("lena.jpg");
-	namedWindow("image", WINDOW_NORMAL);
-	imshow("image", img);
+	Mat sudoku = imread("sudoku.jpeg", 0);
+
+	//Mat sudoku;
+	//pyrDown(imread("sudoku3.jpg", 0), sudoku);
+
+	Mat original = sudoku.clone();
+
+	// Create a duplicate. We'll try to extract grid lines in this image
+	Mat outerBox = Mat(sudoku.size(), CV_8UC1);
+
+
+	//erode(sudoku, sudoku, kernel);
+
+	GaussianBlur(sudoku, sudoku, Size(9, 9), 0);
+	adaptiveThreshold(sudoku, outerBox, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 11, 2);
+
+	bitwise_not(outerBox, outerBox);
+
+	Mat kernel = (Mat_<uchar>(3, 3) << 0, 1, 0, 1, 1, 1, 0, 1, 0);
+	dilate(outerBox, outerBox, kernel);
+
+	imshow("image", outerBox);
 	waitKey(0);
 	return 0;
 }
