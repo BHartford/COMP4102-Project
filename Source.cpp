2434d48b1096f185dc149b8d3fc34e30433aa6a8
Brandon <hartbrand99@gmail.com>
2020-03-01 16:08:18 -0500

    Detect corners of sudoku grid

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,57 @@
 #include<iostream>
 using namespace std;
 using namespace cv;
+
+Point getTopLeft(vector<Point> points) {
+	Point topLeftPoint = points[0];
+	int smallestSum = points[0].x + points[0].y;
+	for (int i = 0; i < points.size(); i++) {
+		if ((points[i].x + points[i].y) < smallestSum) {
+			topLeftPoint = points[i];
+			smallestSum = points[i].x + points[i].y;
+		}
+	}
+	return topLeftPoint;
+}
+
+Point getBottomRight(vector<Point> points) {
+	Point bottomRightPoint = points[0];
+	int largestSum = points[0].x + points[0].y;
+	for (int i = 0; i < points.size(); i++) {
+		if ((points[i].x + points[i].y) > largestSum) {
+			bottomRightPoint = points[i];
+			largestSum = points[i].x + points[i].y;
+		}
+	}
+	return bottomRightPoint;
+}
+
+Point getTopRight(vector<Point> points, Point topLeft, Point bottomRight) {
+	Point topRightPoint = points[0];
+	int smallestDif = abs(points[0].y - bottomRight.y) + abs(points[0].x - topLeft.x);
+	for (int i = 0; i < points.size(); i++) {
+		int dif = abs(points[i].y - bottomRight.y) + abs(points[i].x - topLeft.x);
+		if (dif > smallestDif) {
+			topRightPoint = points[i];
+			smallestDif = dif;
+		}
+	}
+	return topRightPoint;
+}
+
+Point getBottomLeft(vector<Point> points, Point topLeft, Point bottomRight) {
+	Point bottomLeftPoint = points[0];
+	int smallestDif = abs(points[0].y - topLeft.y) + abs(points[0].x - bottomRight.x);
+	for (int i = 0; i < points.size(); i++) {
+		int dif = abs(points[i].y - topLeft.y) + abs(points[i].x - bottomRight.x);
+		if (dif > smallestDif) {
+			bottomLeftPoint = points[i];
+			smallestDif = dif;
+		}
+	}
+	return bottomLeftPoint;
+}
+
 int main()
 {
 	Mat sudoku = imread("sudoku.jpeg", 0);
@@ -10,22 +61,62 @@ int main()
 	//pyrDown(imread("sudoku3.jpg", 0), sudoku);
 
 	Mat original = sudoku.clone();
+	Mat img = Mat::zeros(original.rows, original.cols, CV_8UC3);
 
 	// Create a duplicate. We'll try to extract grid lines in this image
-	Mat outerBox = Mat(sudoku.size(), CV_8UC1);
+	Mat outerBox = sudoku.clone();
 
 
 	//erode(sudoku, sudoku, kernel);
 
 	GaussianBlur(sudoku, sudoku, Size(9, 9), 0);
-	adaptiveThreshold(sudoku, outerBox, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 11, 2);
+	adaptiveThreshold(sudoku, outerBox, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 11, 2);
 
 	bitwise_not(outerBox, outerBox);
 
 	Mat kernel = (Mat_<uchar>(3, 3) << 0, 1, 0, 1, 1, 1, 0, 1, 0);
-	dilate(outerBox, outerBox, kernel);
+	//dilate(outerBox, outerBox, kernel);
 
-	imshow("image", outerBox);
+	Canny(outerBox, outerBox, 30, 60, 3);
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> output;
+
+	findContours(outerBox, contours, output, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+
+	int finalIndex;
+	int index = 0;
+	int largestArea = 0;
+	for (; index >= 0; index = output[index][0])
+	{
+		int area = contourArea(contours[index]);
+		if (area > largestArea) {
+			largestArea = area;
+			finalIndex = index;
+		}
+	}
+
+	Scalar color(rand() & 255, rand() & 255, rand() & 255);
+	Scalar colore(0,0,255);
+	drawContours(img, contours, finalIndex, color, FILLED, 8, output);
+
+	vector<Point> hull(contours[finalIndex].size());
+	convexHull(Mat(contours[finalIndex]), hull, false);
+
+	Point p1 = getTopLeft(hull);
+	Point p2 = getBottomRight(hull);
+	Point p3 = getTopRight(hull, p1, p2);
+	Point p4 = getBottomLeft(hull, p1, p2);
+	circle(img, p1, 3, colore, -1);
+	circle(img, p2, 3, colore, -1);
+	circle(img, p3, 3, colore, -1);
+	circle(img, p4, 3, colore, -1);
+
+	for (int i = 0; i < hull.size(); i++) {
+		cout << hull[i] << "\n";
+	}
+
+	imshow("image", img);
 	waitKey(0);
 	return 0;
 }
